67bf56928eacaffa69fadeaf2bb05a902f4eab2e
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-04 22:49:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.get('/', function (req, res) {
 });
 
 app.get("/article-one",function(req,res){
-    res.send("article one is requested and will be sent soon")
-})
+    res.sendFile(path.join(_dirname,'ui',article-one.html));
+});
 
 app.get("/article-two",function(req,res){
     res.send("article two is requested and will be sent soon")
-})
+});
 
 app.get("/article-three",function(req,res){
     res.send("article three is requested and will be sent soon")
-})
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
