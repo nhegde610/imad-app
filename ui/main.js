2c339a3d90cd55225a92c4a67880844cfabdcb40
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-05 23:32:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,4 @@
-/*console.log('Loaded!');
-
-
-var element = document.getElementById("main");
-element.innerHTML = "New Value";
-
-var image = document.getElementById("madi");
-var margin = 0;
-function moveRight(){
-    margin = margin + 1;
-    image.style.marginRight = margin + 'px';
-}
-image.onclick = function() {
-    var interval = setInterval(moveRight,100);
-    //image.style.marginLeft = '110px';
-};*/
-var button = document.getElementById("button");
+var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function(){
     
@@ -26,8 +10,8 @@ button.onclick = function(){
         if(request.status === 200){
            var counter =  request.responseText;
            
-           var count = document.getElementById("count");
-           count.innerHTML = counter.toString();
+           var span = document.getElementById("count");
+           span.innerHTML = counter.toString();
         }
     }
     
