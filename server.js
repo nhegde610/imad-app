8541a8d2f9ee5d6ea97ac8fad676de5a6c4b4252
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-06 01:04:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names= [];
-app.get('/submit-name/',function(req,res){
+app.get('/submit-name',function(req,res){
    var name= req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
