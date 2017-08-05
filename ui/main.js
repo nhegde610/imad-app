de17d7cd5e21afb39577eefd809704a013466eea
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-05 07:15:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ var element = document.getElementById("main");
 element.innerHTML = "New Value";
 
 var image = document.getElementById("madi");
+
 image.onclick = function() {
     image.style.marginLeft = "10";
-}
+};
