4d254fd86f5f4aa934c809fb74dd63e1491460a3
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-20 21:51:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function hash(input,salt){
 }
 
 app.get('/hash/:input',function(req,res){
-    var hasedString = hash(Req.params.input,'this-is-some-random-string');
+    var hasedString = hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
 });
 var pool = new Pool(config);
