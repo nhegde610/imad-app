baa9ef92c6609c488067e533f2624a049601a30a
Niranjan Hegde <niranjanhegde2015@gmail.com>
2017-08-20 21:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkd2fSync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
